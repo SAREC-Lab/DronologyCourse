1ec49cd7f61ccf2aade4944291a54cb80bcd9a66
mvier <github.123@vierhauser.net>
2017-04-10 15:09:39 -0400

    dummy unit test classes added

---
+++ edu.nd.dronology.core/test/edu/nd/dronology/core/DummyTest1.java
@@ -0,0 +1,19 @@
+package edu.nd.dronology.core;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DummyTest1 {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ edu.nd.dronology.core/test/edu/nd/dronology/core/DummyTest2.java
@@ -0,0 +1,19 @@
+package edu.nd.dronology.core;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DummyTest2 {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
