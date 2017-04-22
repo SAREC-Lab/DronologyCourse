af13779645405c3faccf5f3d60cf023103731c13
mvier <github.123@vierhauser.net>
2017-04-22 17:23:07 -0400

    new Unit Test added

---
+++ edu.nd.dronology.core/test/edu/nd/dronology/core/fleet_manager/TestRuntimeDroneTypes.java
@@ -0,0 +1,84 @@
+package edu.nd.dronology.core.fleet_manager;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import edu.nd.dronology.core.flight_manager.SoloDirector;
+@RunWith(Parameterized.class)
+public class TestRuntimeDroneTypes {
+
+	
+
+	    @Parameterized.Parameters
+	    public static List<Object[]> data() {
+	        return Arrays.asList(new Object[10][0]);
+	    }
+	
+	
+	
+	
+	
+	SoloDirector testInstance;
+
+	@Before
+	public void setUp() throws Exception {
+
+	}
+
+	@Test
+	public void testgetInstance() {
+
+		final CyclicBarrier gate = new CyclicBarrier(3);
+
+		Thread t1 = new Thread() {
+
+			public void run() {
+				try {
+					gate.await();
+				} catch (InterruptedException | BrokenBarrierException e) {
+					e.printStackTrace();
+				}
+				instance1 = RuntimeDroneTypes.getInstance();
+			}
+		};
+		Thread t2 = new Thread() {
+			public void run() {
+				try {
+					gate.await();
+				} catch (InterruptedException | BrokenBarrierException e) {
+					e.printStackTrace();
+				}
+				instance2 = RuntimeDroneTypes.getInstance();
+			}
+		};
+
+		t1.start();
+		t2.start();
+		
+		try {
+			gate.await();
+			Thread.sleep(500);
+		} catch (InterruptedException | BrokenBarrierException e) {
+			e.printStackTrace();
+		}
+		
+		assertNotNull(instance1);
+		assertNotNull(instance2);
+		assertEquals(instance1, instance2);
+
+	}
+
+	private RuntimeDroneTypes instance1;
+	private RuntimeDroneTypes instance2;
+
+}
