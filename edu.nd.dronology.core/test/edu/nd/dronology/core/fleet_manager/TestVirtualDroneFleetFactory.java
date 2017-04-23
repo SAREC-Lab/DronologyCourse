0dc6ea9fe1a0d8c7410bc797a881a7f9541d2e36
mvier <github.123@vierhauser.net>
2017-04-23 16:32:32 -0400

    update Unit-Test due to api change

--- edu.nd.dronology.core/test/edu/nd/dronology/core/fleet_manager/TestVirtualDroneFleetFactory.java
+++ edu.nd.dronology.core/test/edu/nd/dronology/core/fleet_manager/TestVirtualDroneFleetFactory.java
@@ -2,9 +2,6 @@ package edu.nd.dronology.core.fleet_manager;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 import org.junit.Before;
@@ -12,11 +9,6 @@ import org.junit.Test;
 
 import edu.nd.dronology.core.drones_runtime.ManagedDrone;
 import edu.nd.dronology.core.drones_runtime.VirtualDrone;
-import edu.nd.dronology.core.drones_runtime.iDrone;
-import edu.nd.dronology.core.gui_middleware.DroneStatus;
-import edu.nd.dronology.core.home_bases.BaseManager;
-import edu.nd.dronology.core.utilities.Coordinates;
-import edu.nd.dronology.core.zone_manager.FlightZoneException;
 
 public class TestVirtualDroneFleetFactory {
 
@@ -24,24 +16,18 @@ public class TestVirtualDroneFleetFactory {
 
 	@Before
 	public void setUp() throws Exception {
-		try {
-			testInstance = new VirtualDroneFleetFactory(3, new BaseManager(5));
-		} catch (FlightZoneException e) {
-			e.printStackTrace();
-		}
+		testInstance = new VirtualDroneFleetFactory();
 
 	}
 
-	@Test(expected = IllegalArgumentException.class)
-	public void testCreateWithNull() {
-		new VirtualDroneFleetFactory(1, null);
-
-	}
 
 	@Test
-	public void testGetDrones() {
+	public void testInitializeDrone() {
 		List<ManagedDrone> drones = testInstance.getDrones();
-		assertEquals(3, drones.size());
+		assertEquals(0, drones.size());
+		ManagedDrone d = testInstance.initializeDrone("1", "abc", 12, 12, 12);
+		//TODO.. add test
+		
 	}
 
 	@Test(expected = UnsupportedOperationException.class)
