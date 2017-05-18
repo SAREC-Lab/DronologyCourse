ba2047ede45cf726aebb7373cebc94ecf2cb07c6
github@vierhauser.net <github@vierhauser.net>
2017-05-18 10:13:47 -0400

    eclipse RCP application added to github

---
+++ eclipse/edu.nd.dronology.ui.cc.application/src/edu/nd/dronology/ui/cc/application/constants/CommandConstants.java
@@ -0,0 +1,15 @@
+package edu.nd.dronology.ui.cc.application.constants;
+
+public class CommandConstants {
+
+	public static final String SEARCH_ONLINE_COMMAND = "";
+	public static final String SWTICH_PERSPECTIVE_COMMAND = "edu.nd.dronology.ui.cc.commands.switchperspective";
+	public static final String IMPORT_COMMAND = null;
+	public static final String EXPORT_COMMAND = null;
+	public static final String ADD_ENTRY_COMMAND = null;
+	public static final String SHOW_SETTINGS_COMMAND ="edu.nd.dronology.commands.settings"; 
+	//public static final String SHOW_ABOUT_COMMAND = "edu.nd.dronology.commands.about";
+	public static final String SWTICH_THEME_COMMAND = null;
+	public static final String TRANSMIT_COMMAND = "edu.nd.dronology.commands.transmit"; 
+
+}
---
+++ eclipse/edu.nd.dronology.ui.cc.application/src/edu/nd/dronology/ui/cc/application/constants/PerspectiveConstants.java
@@ -0,0 +1,20 @@
+package edu.nd.dronology.ui.cc.application.constants;
+
+public class PerspectiveConstants {
+
+	public static final String LAUNCHER_PERSPECTIVE = "edu.nd.dronology.ui.cc.perspective.launch";
+	
+	public static final String FLIGHT_ROUTEPLANNING_PERSPECTIVE = "edu.nd.dronology.ui.cc.perspective.routeplanning";
+	
+	public static final String TAKEOFF_PERSPECTIVE = "edu.nd.dronology.ui.cc.perspective.takeoff";
+	
+	public static final String SIMULATOR_PERSPECTIVE = "edu.nd.dronology.ui.cc.perspective.simulator";
+	
+	public static final String SIMULATOR__SCENARIO_PERSPECTIVE = "edu.nd.dronology.ui.cc.perspective.simulatorscenario";
+	
+	public static final String SPECIFICATION_PERSPECTIVE = "edu.nd.dronology.ui.cc.perspective.specification";
+	                                                    
+	
+	public static final String MONITOR_PERSPECTIVE = "edu.nd.dronology.ui.cc.perspective.monitor";
+
+}
