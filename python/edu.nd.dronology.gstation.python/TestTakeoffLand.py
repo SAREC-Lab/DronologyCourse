dd8f8f7ef93f65355e191dc9593680576e04a677
jhuseman <jhuseman@nd.edu>
2017-06-07 11:45:24 -0400

    Added a few dronekit test files

---
+++ python/edu.nd.dronology.gstation.python/TestTakeoffLand.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+
+from DroneComm import *
+
+drones = [
+	{
+		'type':'SITL',
+		'ConnectionData':{
+			'inst':0,
+			'home':'41.732955,-86.180886,0,0',
+		},
+	},
+]
+
+# drones = [
+	# {
+		# 'type':'physical',
+		# 'ConnectionData':{
+			# 'ConnectionString':'192.168.42.1:1234',
+		# },
+	# },
+# ]
+
+current_drone_id = 0
+drones = {}
+for drone in drones:
+	new_drone = DroneComm(drone['type'],drone['ConnectionData'])
+	drones[current_drone_id] = new_drone
+	current_drone_id = current_drone_id + 1
+
+drones[0].takeoff(20)
+loc = drones[0].getLocation()
+# drones[0].gotoLocation(Coordinate(41.7,-86.2,20))
+drones[0].gotoLocation(loc)
+drones[0].setMode('LAND')
