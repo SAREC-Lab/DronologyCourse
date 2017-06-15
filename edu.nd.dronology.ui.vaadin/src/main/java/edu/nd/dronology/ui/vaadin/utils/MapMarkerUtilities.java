c38030d3d6e7675e6c7d349a327a9d7ec29408bb
pfalvey <pfalvey@nd.edu>
2017-06-15 17:01:55 -0400

    UAV-175 Add functionality to move waypoints

--- edu.nd.dronology.ui.vaadin/src/main/java/edu/nd/dronology/ui/vaadin/utils/MapMarkerUtilities.java
+++ edu.nd.dronology.ui.vaadin/src/main/java/edu/nd/dronology/ui/vaadin/utils/MapMarkerUtilities.java
@@ -5,9 +5,12 @@ import java.util.ArrayList;
 import org.vaadin.addon.leaflet.LMap;
 import org.vaadin.addon.leaflet.LMarker;
 import org.vaadin.addon.leaflet.LPolyline;
+import org.vaadin.addon.leaflet.LTileLayer;
 import org.vaadin.addon.leaflet.shared.Point;
 
+import com.vaadin.ui.Component;
 import com.vaadin.ui.Grid;
+import com.vaadin.ui.Notification;
 
 /**
  * This is the class that contains all logic for plotting flight routes.
@@ -20,6 +23,7 @@ public class MapMarkerUtilities {
 	ArrayList<Point> mapPoints = new ArrayList<Point>();
 	ArrayList<LPolyline> polylines = new ArrayList<LPolyline>();
 	private Grid<Point> grid = new Grid<>();
+	private Point previou;
 	
 	public MapMarkerUtilities() {
 		grid.addColumn(Point::getLat).setCaption("Latitude");
@@ -28,15 +32,39 @@ public class MapMarkerUtilities {
 
 	public void addPin(Point point, LMap map) {
 		LMarker leafletMarker = new LMarker(point);
-		
         leafletMarker.addClickListener(event -> {
         	map.removeComponent(leafletMarker);
-        	mapPoints.remove(point);
+        	mapPoints.remove(leafletMarker.getPoint());
         	removeAllLines(polylines, map);
         	grid.setItems(mapPoints);
         	polylines = drawLines(mapPoints, map);
         });
         
+        /**
+         * Drag End Listener is a listener that updates the path if a waypoint is moved.
+         * The leafletMarker description is used to keep track of the previous point.
+         * @author Patrick Falvey
+         */
+        leafletMarker.addDragEndListener(event-> {
+        	int index = -1;
+        	if (leafletMarker.getDescription().equals(""))
+        		index = mapPoints.indexOf(point);
+        	else {
+        		for (int i = 0; i < mapPoints.size(); i++){
+        			String latlon = Double.toString(mapPoints.get(i).getLat());
+        			latlon += Double.toString(mapPoints.get(i).getLon());
+        			if (latlon.equals(leafletMarker.getDescription())){
+        				index = i;
+        			}
+        		}
+        	}
+        	String description = (Double.toString(leafletMarker.getPoint().getLat()) + Double.toString(leafletMarker.getPoint().getLon()));
+        	leafletMarker.setDescription(description);
+        	mapPoints.set(index, leafletMarker.getPoint());
+        	removeAllLines(polylines, map);
+        	polylines = drawLines(mapPoints, map);
+        	grid.setItems(mapPoints);
+        });
 		map.addComponent(leafletMarker);
 		mapPoints.add(point);
 		removeAllLines(polylines, map);
