9356908face442062bc28d2943b45278c4f41a63
github@vierhauser.net <github@vierhauser.net>
2017-06-22 16:05:49 -0400

    code cleanup and new timestamp class added

--- edu.nd.dronology.core/src/edu/nd/dronology/core/air_traffic_control/DroneSeparationMonitor.java
+++ edu.nd.dronology.core/src/edu/nd/dronology/core/air_traffic_control/DroneSeparationMonitor.java
@@ -3,7 +3,6 @@ package edu.nd.dronology.core.air_traffic_control;
 import java.util.ArrayList;
 import java.util.List;
 
-import edu.nd.dronology.core.fleet.DroneFleetManager;
 import edu.nd.dronology.core.vehicle.ManagedDrone;
 import net.mv.logging.ILogger;
 import net.mv.logging.LoggerProvider;
