68116ecb26705af97cf7ee500e7d7f9c22752eaf
Michael Murphy <murphym18@gmail.com>
2017-06-29 13:48:31 -0700

    Add 3 distance tests to abstract position UAV-286
    Need to be sure that the abstract position class is working. Add three
    tests that make sure the distance from end zone to end zone at nd
    stadium is 91.44 meters.
    
    Also change the implementation of distance make it clear to those
    reading the source code that distance is returning laser distance.

--- edu.nd.dronology.core/src/edu/nd/dronology/core/util/AbstractPosition.java
+++ edu.nd.dronology.core/src/edu/nd/dronology/core/util/AbstractPosition.java
@@ -6,7 +6,7 @@ public abstract class AbstractPosition {
 	public abstract LlaCoordinate toLlaCoordinate();
 	
 	public double distance(AbstractPosition other) {
-		return this.toNVector().distance(other.toNVector());
+		return NVector.laserDistance(this.toNVector(), other.toNVector());
 	}
 	
 	public double travelDistance(AbstractPosition other) {
