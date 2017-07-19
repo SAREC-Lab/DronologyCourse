61640176ff0bf0936f027da18d50489b01582006
github@vierhauser.net <github@vierhauser.net>
2017-07-19 18:18:01 -0400

    Code cleanup

--- edu.nd.dronology.services.launch/src/edu/nd/dronology/services/launch/DronologyServiceRunner.java
+++ edu.nd.dronology.services.launch/src/edu/nd/dronology/services/launch/DronologyServiceRunner.java
@@ -46,12 +46,12 @@ public class DronologyServiceRunner {
 		//	new SafetyCaseValidator(SafetyCaseGeneration.getUAVSafetyCase()).validate();
 			MAVLinkUAVConnector groundStation = new MAVLinkUAVConnector("HUEY", "huey.cse.nd.edu", 1234);
 			
-			//MAVLinkUAVConnector groundStation2 = new MAVLinkUAVConnector("ILIA", "ilia.cse.nd.edu", 1234);
+			MAVLinkUAVConnector groundStation2 = new MAVLinkUAVConnector("ILIA", "ilia.cse.nd.edu", 1234);
 			
 			RemoteManager.getInstance().contributeService(IDroneSafetyRemoteService.class, DroneSafetyServiceRemoteFacade.getInstance());
 			
 			runtimeMode.registerCommandHandler(groundStation);
-			//runtimeMode.registerCommandHandler(groundStation2);
+			runtimeMode.registerCommandHandler(groundStation2);
 
 			groundStation.registerMonitoringMessageHandler(UAVMonitoringManager.getInstance());
 			//groundStation.registerSafetyValidator(SafetyCaseValidationManager.getInstance());
