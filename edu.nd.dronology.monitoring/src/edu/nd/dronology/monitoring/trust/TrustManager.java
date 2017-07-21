343ba5e81cc0366ce829b075cbcb32deae101033
seanbayley <seanbayley@gmail.com>
2017-07-21 11:20:48 -0400

    no message

--- edu.nd.dronology.monitoring/src/edu/nd/dronology/monitoring/trust/TrustManager.java
+++ edu.nd.dronology.monitoring/src/edu/nd/dronology/monitoring/trust/TrustManager.java
@@ -2,6 +2,20 @@ package edu.nd.dronology.monitoring.trust;
 
 import java.rmi.RemoteException;
 import java.rmi.server.RemoteObject;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+import java.util.HashMap;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.lang.IllegalArgumentException;
+import java.nio.file.Paths;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
 
 import edu.nd.dronology.monitoring.service.DroneSafetyService;
 import edu.nd.dronology.monitoring.service.IMonitoringValidationListener;
@@ -9,18 +23,56 @@ import edu.nd.dronology.monitoring.validation.ValidationResult.Result;
 import net.mv.logging.ILogger;
 import net.mv.logging.LoggerProvider;
 
+
+/**
+ * An entity used to determine the monitoring frequency of 
+ * vehicles within the infrastructure.  
+ * 
+ * @author seanbayley
+ *
+ */
 public class TrustManager {
 	private static final ILogger LOGGER = LoggerProvider.getLogger(TrustManager.class);
 	private static volatile TrustManager INSTANCE = null;
 
-	public TrustManager() {
+	private static final String PATH_TO_HISTORY = 
+			Paths.get("src", "edu", "nd", "dronology", "monitoring", "trust", "history.json").toString();
+	private static final double FORGETTING_FACTOR = 0.9;
 
+	private Map<String, VehicleReputation> history;
+	private String pathToHistory; 
+	private double forgettingFactor; 
+
+
+	public TrustManager() {
+		this(PATH_TO_HISTORY, FORGETTING_FACTOR);
+	}
+
+	public TrustManager(String pathToHistory) {
+		this(pathToHistory, FORGETTING_FACTOR);
+	}
+
+	public TrustManager(double forgettingFactor) {
+		this(PATH_TO_HISTORY, forgettingFactor);
+	}
+
+	public TrustManager(String pathToHistory, double forgettingFactor) {
+		this.pathToHistory = pathToHistory;
+		this.forgettingFactor = forgettingFactor;
+		this.history = new HashMap<String, VehicleReputation>();
+		try {
+			Gson gson = new Gson();
+			JsonReader json = new JsonReader(new FileReader(pathToHistory));
+			this.history = gson.fromJson(json, new TypeToken<Map<String, VehicleReputation>>(){}.getType());
+
+		} catch (JsonIOException | JsonSyntaxException | FileNotFoundException e) {
+			LOGGER.error(e.getMessage());
+		}
 	}
 
 	/**
 	 * The singleton instance.
 	 */
-
 	public static TrustManager getInstance() {
 
 		if (INSTANCE == null) {
@@ -34,26 +86,166 @@ public class TrustManager {
 	}
 
 	public void initialize() {
-		DroneSafetyService.getInstance().addValidationListener(new InternalMonitoringEvalListener());
+        DroneSafetyService.getInstance().addValidationListener(new InternalMonitoringEvalListener());
 
 	}
 
 	public void initializeUAV(String uavid) {
 		LOGGER.info("new uav initialized: " + uavid);
 	}
+	
+	/**
+	 * Called by InternalMonitoringEvalListener when a constraint is evaluated
+	 * @param vid the vehicle id
+	 * @param assumptionid the assumption id
+	 * @param success the result (1 or -1)
+	 * @throws IllegalArgumentException
+	 */
+	public void constraintEvaluated(String vid, String assumptionid, int success) 
+			throws IllegalArgumentException {
+		if (!history.containsKey(vid)) 
+			throw new IllegalArgumentException(
+					String.format("vehicle %s not recognized", vid));
 
-	private static class InternalMonitoringEvalListener extends RemoteObject implements IMonitoringValidationListener {
+		history.get(vid).getReputationRating(assumptionid).addFeedback(success);
+	}
+	
+	/**
+	 * Determine the reputation rating of a vehicle.
+	 * @param vid the vehicle id
+	 * @return the reputation rating r (0 < r < 1)
+	 */
+	public double getReputationRating(String vid) {
+		// TODO: how do we want to combine reputation ratings from different assumptions?
+		return 0.0;
+	}
+
+	@Override
+	public String toString() {
+		return history.entrySet().stream().map(
+				entry -> String.format("%s: %s", 
+						entry.getKey(), 
+						entry.getValue().toString())).
+				collect(Collectors.joining(System.getProperty("line.separator")));
+	}
+
+	
+	public class VehicleReputation {
+		private Map<String, ReputationRating> assumptions;
+
+		public ReputationRating getReputationRating(String assumptionId) 
+				throws IllegalArgumentException {
+			if (!assumptions.containsKey(assumptionId))
+				throw new IllegalArgumentException(
+						String.format("unrecognized assumptionId %s", assumptionId));
+			return assumptions.get(assumptionId);
+		}
+		
 		/**
-			 * 
-			 */
-		private static final long serialVersionUID = -3045122339587951628L;
+		 * Get all assumption ids registered with this vehicle.
+		 * @return
+		 */
+		public Iterable<String> getAssumptionIds() {
+			return assumptions.keySet();
+		}
+		
+		/**
+		 * Get the assumption K, V pairs.
+		 * @return
+		 */
+		public Iterable<Entry<String, ReputationRating>> getAssumptionEntrySet() {
+			return assumptions.entrySet();
+		}
 
 		@Override
-		public void constraintEvaluated(String uavid, String assumptionid, String message, Result result)
-				throws RemoteException {
-			LOGGER.info("New Eval result:" + uavid + ": " + assumptionid + " --> " + result.name());
-
+		public String toString() {
+			return assumptions.entrySet().stream().map(
+					entry -> String.format("%s: %s", 
+							entry.getKey(), 
+							entry.getValue().toString())).
+					collect(Collectors.joining(", "));
 		}
 	}
 
+	public class ReputationRating {
+		/**
+		 * The ReputationRating is based on the beta reputation system. 
+		 * Josang, Audun, and Roslan Ismail. "The beta reputation system."
+		 * 
+		 * Currently supports the base reputation rating (Equation 5) 
+		 */
+
+		private double r;
+		private double s;
+
+		public double getR() {
+			return r;
+		}
+
+		public double getS() {
+			return s;
+		}
+		
+		/**
+		 * Add feedback based on the result of some "interaction".
+		 *  @param success
+		 *  	1 if the interaction was successful otherwise -1
+		 */
+		public void addFeedback(int success) {
+			if (Math.abs(success) != 1) {
+				throw new IllegalArgumentException(
+						"the value of \"success\" should be 1 or -1.");
+			}
+			
+			if (success == -1) {
+				s += 1;
+			}
+			else {
+				r += 1;
+			}
+
+		}
+		
+		/**
+		 * Determine the reputation given:
+		 * 	 
+		 * 	 r (the number of positive results), and 
+		 *   s (the number of negative results). 
+		 *   
+		 * The rating is calculated using Equation 5.
+		 * Ratings range from (0, 1). 
+		 */
+		public double getReputationRating() {
+			return (r + 1) / (r + s + 2); 
+		}
+
+		@Override
+		public String toString() {
+			return String.format("%f", getReputationRating());
+		}
+	}
+	
+	private static class InternalMonitoringEvalListener extends RemoteObject 
+		implements IMonitoringValidationListener {
+		
+        private static final long serialVersionUID = -3045122339587951628L;
+		@Override
+		public void constraintEvaluated(String vid, String assumptionid, String message, Result result)
+				throws RemoteException {
+			// TODO: figure out what whether or not this was a success from the result
+			int success = 1;
+			try {
+				TrustManager.getInstance().constraintEvaluated(vid, assumptionid, success);
+			}
+			catch (IllegalArgumentException e) {
+				LOGGER.warn(e.getMessage());
+			}
+			
+		}
+	
+	}
+
 }
+
+
+
