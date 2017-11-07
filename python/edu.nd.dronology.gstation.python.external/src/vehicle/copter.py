c4d689bfa06935e38b433db4def4299e4954e7f4
Sean Bayley <seanbayley@gmail.com>
2017-11-07 14:57:34 -0500

    refactor vehicle.py into it's own package

--- python/edu.nd.dronology.gstation.python.external/src/vehicle/copter.py
+++ python/edu.nd.dronology.gstation.python.external/src/vehicle/copter.py
@@ -1,84 +1,13 @@
-import util
 import threading
+import util
 import dronekit
 import dronekit_sitl
-from communication import message, command
-from common import *
 from pymavlink import mavutil
-# from
+from common import *
+from communication import message, command
+from vehicle import VehicleControl
 
 _LOG = util.get_logger()
-
-
-
-def make_mavlink_command(cmd, trg_sys=0, trg_component=0, seq=0,
-                         frame=mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
-                         param1=0, param2=0, param3=0, param4=0,
-                         lat_or_param5=0, lon_or_param6=0, alt_or_param7=0):
-    """
-    Make a new mavlink command.
-
-    :param cmd:
-    :param trg_sys:
-    :param trg_component:
-    :param seq:
-    :param frame:
-    :param param1:
-    :param param2:
-    :param param3:
-    :param param4:
-    :param lat_or_param5:
-    :param lon_or_param6:
-    :param alt_or_param7:
-    :return:
-    """
-    cmd_args = [trg_sys, trg_component,
-                seq,
-                frame,
-                cmd,
-                0, 0,
-                param1, param2, param3, param4,
-                lat_or_param5, lon_or_param6, alt_or_param7]
-
-    return dronekit.Command(*cmd_args)
-
-
-class VehicleControl(object):
-    def __init__(self, handshake_msg_queue, state_msg_queue, vehicle_id=None, state_interval=1.0):
-        self._vehicle = None
-        self._vid = vehicle_id
-        self._handshake_out_msgs = handshake_msg_queue
-        self._state_out_msgs = state_msg_queue
-        self._state_t = state_interval
-        self._state_msg_timer = None
-        self._responsive = True
-
-    def update_state_interval(self, state_interval):
-        self._state_msg_timer.stop()
-        self._state_msg_timer.set_interval(state_interval)
-        self._state_msg_timer.start()
-
-    def send_state_message(self):
-        msg = self.gen_state_message()
-        self._state_out_msgs.put_message(msg)
-
-    def gen_state_message(self):
-        raise NotImplementedError
-
-    def get_vehicle(self):
-        return self._vehicle
-
-    def get_vehicle_id(self):
-        return self._vid
-
-    def connect_vehicle(self, **kwargs):
-        raise NotImplementedError
-
-    def handle_command(self, cmd):
-        raise NotImplementedError
-
-    def stop(self):
-        raise NotImplementedError
 
 
 class CopterControl(VehicleControl):
@@ -141,26 +70,51 @@ class ArduCopter(CopterControl):
             '3D_ACCEL': mavutil.mavlink.MAV_SYS_STATUS_SENSOR_3D_ACCEL
         }
 
-    def get_location(self):
-        lla = self._vehicle.location.global_relative_frame
+    def get_location(self, global_relative=True):
+        """
+        Get the current lat,lon,alt in the global relative frame (altitude wrt to starting altitude)
+        :return:
+        """
+        if global_relative:
+            lla = self._vehicle.location.global_relative_frame
+        else:
+            lla = self._vehicle.location.global_frame
+
         return util.Lla(lla.lat, lla.lon, lla.alt)
 
     def handle_command(self, cmd):
-        if not self._vehicle:
-            _LOG.error('Vehicle {} not connected! Ignoring command.'.format(self._vid))
-        elif type(cmd) not in self._cmd_handlers:
-            _LOG.warn('Vehicle {} received unrecognized command \"{}\" for \"{}\" controller!'.format(self._vid,
-                                                                                                      cmd.__class__.__name__,
-                                                                                                      self.__class__.__name__))
-        elif not self._responsive:
-            _LOG.info('Vehicle {} ignoring command: manual control overrides!')
-        else:
-            self._cmd_handlers[type(cmd)](cmd)
+        """
+        Handle a command from dronology
+        :param cmd:
+        :return:
+        """
+        with self._drone_lock:
+            if not self._vehicle:
+                _LOG.error('Vehicle {} not connected! Ignoring command.'.format(self._vid))
+            elif type(cmd) not in self._cmd_handlers:
+                _LOG.warn('Vehicle {} received unrecognized command \"{}\" for \"{}\" '
+                          'controller!'.format(self._vid,
+                                               cmd.__class__.__name__,
+                                               self.__class__.__name__))
+            elif not self._responsive:
+                _LOG.info('Vehicle {} ignoring command: manual control overrides!')
+            else:
+                self._cmd_handlers[type(cmd)](cmd)
 
     def _set_mode(self, cmd):
+        """
+        Set the vehicle mode
+        :param cmd:
+        :return:
+        """
         self.__set_mode(cmd.get_mode())
 
     def __set_mode(self, mode):
+        """
+        Set the vehicle mode to the specified mode
+        :param mode:
+        :return:
+        """
         mode_ = self._vehicle.mode.name
 
         while mode_ != mode:
@@ -168,35 +122,75 @@ class ArduCopter(CopterControl):
             self._vehicle.mode = dronekit.VehicleMode(mode)
 
     def _takeoff(self, cmd):
+        """
+        Takeoff
+        :param cmd:
+        :return:
+        """
         self.__takeoff(cmd.get_altitude())
 
     def __takeoff(self, alt):
-        self.__set_mode('STABILIZE')
+        """
+        Takeoff to the specified altitude
+        :param alt:
+        :return:
+        """
+        self.__set_mode('GUIDED')
         self._set_armed(armed=True)
-        self.__set_mode('AUTO')
         self._vehicle.simple_takeoff(alt)
         time.sleep(2.0)
 
     def _goto_lla(self, cmd):
+        """
+        Go to a location
+        :param cmd:
+        :return:
+        """
         lat, lon, alt = cmd.get_lla().as_array()
         self.__goto_lla(lat, lon, alt)
 
     def __goto_lla(self, lat, lon, alt):
+        """
+        Go to the specified lat, lon, alt
+        :param lat:
+        :param lon:
+        :param alt:
+        :return:
+        """
         self._vehicle.simple_goto(dronekit.LocationGlobalRelative(lat, lon, alt))
 
     def _land(self, cmd=None):
+        """
+        Land the vehicle
+        :param cmd:
+        :return:
+        """
         self.__land()
 
     def __land(self):
+        """
+        Land the vehicle
+        :return:
+        """
         self.__set_mode('LAND')
 
         while abs(self._vehicle.location.global_relative_frame.alt - 1) > 1:
             time.sleep(2)
 
     def _set_ground_speed(self, cmd):
+        """
+        Set the ground speed
+        :param cmd:
+        :return:
+        """
         self.__set_ground_speed(cmd.get_speed())
 
     def __set_ground_speed(self, speed):
+        """
+        Set the ground speed
+        :param speed:
+        :return:
+        """
         msg = self._vehicle.message_factory.command_long_encode(
             0, 0,  # target system, target component
             mavutil.mavlink.MAV_CMD_DO_CHANGE_SPEED,  # command
@@ -211,9 +205,21 @@ class ArduCopter(CopterControl):
         self._vehicle.flush()
 
     def _set_velocity(self, cmd):
+        """
+        Set the velocity
+        :param cmd:
+        :return:
+        """
         self.__set_velocity(*cmd.get_ned())
 
     def __set_velocity(self, north, east, down):
+        """
+        Set the velocity
+        :param north:
+        :param east:
+        :param down:
+        :return:
+        """
         msg = self._vehicle.message_factory.set_position_target_local_ned_encode(
             0,  # time_boot_ms (not used)
             0, 0,  # target system, target component
@@ -228,9 +234,21 @@ class ArduCopter(CopterControl):
         self._vehicle.flush()
 
     def _set_home_location(self, cmd):
+        """
+        Set the home location.
+        :param cmd:
+        :return:
+        """
         self.__set_home_location(*cmd.get_lla().as_array())
 
     def __set_home_location(self, lat, lon, alt):
+        """
+        Set the home location
+        :param lat:
+        :param lon:
+        :param alt:
+        :return:
+        """
         self._vehicle.send_mavlink(self._vehicle.message_factory.command_long_encode(
             0, 0,  # target system, target component
             mavutil.mavlink.MAV_CMD_DO_SET_HOME,  # command
@@ -241,6 +259,11 @@ class ArduCopter(CopterControl):
         self._vehicle.flush()
 
     def _set_armed(self, armed=True):
+        """
+        Arm or disarm the vehicle.
+        :param armed:
+        :return:
+        """
         if self._vehicle.armed != armed:
             if armed:
                 while not self._vehicle.is_armable:
@@ -253,22 +276,26 @@ class ArduCopter(CopterControl):
                 time.sleep(0.1)
 
     def _get_armed(self):
+        """
+        Determine whether the vehicle is armed
+        :return:
+        """
         return self._vehicle.armed
 
     def connect_vehicle(self, vehicle_type=None, vehicle_id=None, ip=None, instance=0, ardupath=ARDUPATH, rate=10,
                         home=(41.519412, -86.239830, 0, 0), baud=57600, speedup=1.0, async=True):
         """
-        Connect to a SITL vehicle or a real vehicle.
-
+        Connect to a vehicle
         :param vehicle_type:
         :param vehicle_id:
         :param ip:
         :param instance:
         :param ardupath:
-        :param speed:
         :param rate:
         :param home:
         :param baud:
+        :param speedup:
+        :param async:
         :return:
         """
         args = vehicle_type, vehicle_id, ip, instance, ardupath, rate, home, baud, speedup
@@ -277,7 +304,22 @@ class ArduCopter(CopterControl):
         else:
             self._connect_vehicle(*args)
 
-    def _connect_vehicle(self, vehicle_type, vehicle_id, ip, instance, ardupath, rate, home, baud, speedup):
+    def _connect_vehicle(self, vehicle_type, vehicle_id, ip, instance, ardupath, rate, home, baud, speedup,
+                         wait_till_armable=True):
+        """
+        Connect to a vehicle
+        :param vehicle_type:
+        :param vehicle_id:
+        :param ip:
+        :param instance:
+        :param ardupath:
+        :param rate:
+        :param home:
+        :param baud:
+        :param speedup:
+        :param wait_till_armable:
+        :return:
+        """
         status = 0
         vehicle = None
 
@@ -324,17 +366,21 @@ class ArduCopter(CopterControl):
             _LOG.warn('vehicle type {} not supported!'.format(vehicle_type))
             status = -1
 
-        while not vehicle.is_armable:
-            time.sleep(3.0)
+        if wait_till_armable:
+            # Force wait until vehicle is armable. (ensure all pre-flight checks are completed)
+            while not vehicle.is_armable:
+                time.sleep(3.0)
 
-        self._vehicle = vehicle
-        self._vid = vehicle_id
+        with self._drone_lock:
+            self._vehicle = vehicle
+            self._vid = vehicle_id
 
-        lla = vehicle.location.global_relative_frame
-        lat, lon, alt = lla.lat, lla.lon, lla.alt
-        _LOG.debug('Vehicle {} home location is: {}'.format(self._vid, ','.join(map(str, (lat, lon, alt)))))
-        self.__set_home_location(lat, lon, alt)
-        self._register_message_handlers(vehicle)
+            # Ensure home location is properly set
+            lla = vehicle.location.global_relative_frame
+            lat, lon, alt = lla.lat, lla.lon, lla.alt
+            _LOG.debug('Vehicle {} home location is: {}'.format(self._vid, ','.join(map(str, (lat, lon, alt)))))
+            self.__set_home_location(lat, lon, alt)
+            self._register_message_handlers(vehicle)
 
         if status >= 0:
             _LOG.info('Vehicle {} successfully initialized.'.format(self._vid))
@@ -344,10 +390,20 @@ class ArduCopter(CopterControl):
             _LOG.error('Vehicle {} failed to initialize.'.format(self._vid))
 
     def _register_message_handlers(self, vehicle):
+        """
+        Register all vehicle handlers. Add a new method here to register a new handler.
+        :param vehicle:
+        :return:
+        """
         self._register_sys_status_handler(vehicle)
         self._register_mode_change(vehicle)
 
     def _register_sys_status_handler(self, vehicle):
+        """
+        Register sys status listener. If a monitored sensor is not present or unhealthy, report.
+        :param vehicle:
+        :return:
+        """
         @vehicle.on_message('SYS_STATUS')
         def handle_sys_status(_, name, msg):
             for sid, bits in self._sensors.items():
@@ -360,12 +416,32 @@ class ArduCopter(CopterControl):
                     _LOG.warn('Vehicle {} sensor {} not healthy!'.format(self._vid, sid))
 
     def _register_mode_change(self, vehicle):
+        """
+        Register a mode listener on the vehicle. When the mode changes, we should ensure that control is
+        handed back to the RC.
+        :param vehicle:
+        :return:
+        """
         @vehicle.on_attribute('mode')
         def handle_mode_change(_, name, msg):
             _LOG.info('Vehicle {} mode changed to {}, giving up control!'.format(self._vid, name))
-            self._responsive = False
+            with self._drone_lock:
+                mode_name = 'STABILIZE'
+                mode_ = vehicle.mode.name
+
+                # Set mode to stabilize: give back control to the RC
+                while mode_ != mode_name:
+                    mode_ = vehicle.mode.name
+                    vehicle.mode = dronekit.VehicleMode(mode_name)
+
+                # Set response to false.. ignore all future commands that might be sent by Dronology
+                self._responsive = False
 
     def stop(self):
+        """
+        Close connection to vehicle and sitl.
+        :return:
+        """
         if self._vehicle:
             _LOG.info('Closing vehicle {} connection.'.format(self._vid))
             self._vehicle.close()
