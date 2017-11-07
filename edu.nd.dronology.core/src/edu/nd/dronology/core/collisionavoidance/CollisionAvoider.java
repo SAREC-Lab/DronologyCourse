2742dc5c92178a7c04d8e51b1a3dd2444b2fec07
Michael Murphy <murphym18@gmail.com>
2017-11-06 17:30:39 -0800

    Add skeleton code for collision avoidance UAV-433

---
+++ edu.nd.dronology.core/src/edu/nd/dronology/core/collisionavoidance/CollisionAvoider.java
@@ -0,0 +1,11 @@
+package edu.nd.dronology.core.collisionavoidance;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * 
+ */
+public interface CollisionAvoider {
+    public void avoid(ArrayList<Drone> drones);
+}
---
+++ edu.nd.dronology.core/src/edu/nd/dronology/core/collisionavoidance/Drone.java
@@ -0,0 +1,17 @@
+package edu.nd.dronology.core.collisionavoidance;
+
+import edu.nd.dronology.core.util.LlaCoordinate;
+
+/**
+ *
+ * 
+ */
+public class Drone {
+    
+    private LlaCoordinate position;
+    
+    public LlaCoordinate getPosition() {
+        return this.position;
+    }
+    
+}
