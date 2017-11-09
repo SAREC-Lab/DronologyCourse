b3579188f9cb5fa1a8431561bdf779fa815c36d9
Sean Bayley <seanbayley@gmail.com>
2017-11-09 16:52:15 -0500

    begin to add support for RTK injection

--- python/edu.nd.dronology.gstation.python.external/src/main.py
+++ python/edu.nd.dronology.gstation.python.external/src/main.py
@@ -4,12 +4,11 @@ import argparse
 import util
 import signal
 import time
-from common import ARDUPATH
 
 _LOG = util.get_logger()
 
 
-def main(gid, addr, port, drone_configs):
+def main(gid, addr, port, drone_config_path, global_cfg_path):
     dronology_in_msg_queue = communication.core.MessageQueue()
     dronology_handshake_out_msg_queue = communication.core.MessageQueue()
     dronology_state_out_msg_queue = communication.core.MessageQueue()
@@ -20,6 +19,7 @@ def main(gid, addr, port, drone_configs):
                                           dronology_handshake_out_msg_queue, dronology_state_out_msg_queue,
                                           new_vehicle_msg_queue)
 
+    # Ensure everything is shutdown cleanly in case process is killed.
     def shutdown(*args):
         ctrl_station.stop()
         connection.stop()
@@ -27,19 +27,30 @@ def main(gid, addr, port, drone_configs):
     signal.signal(signal.SIGINT, shutdown)
     signal.signal(signal.SIGTERM, shutdown)
 
+    global_cfg = util.load_json(global_cfg_path)
+
+    if 'ardupath' not in global_cfg:
+        _LOG.error('You must specify \"ardupath\" in the global config file at {}'.format(global_cfg_path))
+        exit(1)
+
+    ap = global_cfg['ardupath']
+
     connection.start()
     ctrl_station.start()
     time.sleep(1.0)
 
-    for dc in util.load_drone_configs(drone_configs):
+    # Register vehicles that are specified in a config path (if any)
+    for dc in util.load_json(drone_config_path):
+        dc['ardupath'] = ap
         new_vehicle_msg_queue.put_message(dc)
         time.sleep(2.0)
 
+    # Loop while control station is still operating
     while ctrl_station.is_alive():
         time.sleep(5.0)
 
-    if ctrl_station.is_alive():
-        shutdown(None, None)
+    # Cleanup
+    shutdown()
 
 
 if __name__ == '__main__':
@@ -50,6 +61,7 @@ if __name__ == '__main__':
                         type=str, default='localhost')
     parser.add_argument('-p', '--port',
                         type=int, default=1234)
-    parser.add_argument('-d', '--drone_configs', type=str, default='../cfg/drone_cfgs/default.json')
+    parser.add_argument('-d', '--drone_config', type=str, default='../cfg/drone_cfgs/default.json')
+    parser.add_argument('-c', '--config', type=str, default='../cfg/global_cfg.json')
     args = parser.parse_args()
-    main(args.gcs_id, args.address, args.port, args.drone_configs)
+    main(args.gcs_id, args.address, args.port, args.drone_config, args.config)
--- python/edu.nd.dronology.gstation.python.external/src/util/__init__.py
+++ python/edu.nd.dronology.gstation.python.external/src/util/__init__.py
@@ -8,9 +8,9 @@ def get_logger(name='default_file', p2cfg='../cfg/logging.conf'):
     return etc.get_logger(name=name, p2cfg=p2cfg)
 
 
-def load_drone_configs(p2dcf):
+def load_json(p2f):
     try:
-        with open(p2dcf) as f:
+        with open(p2f) as f:
             return json.load(f)
     except Exception as e:
         print(e)
