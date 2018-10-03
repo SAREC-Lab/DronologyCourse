9b0db18a41256218039f6f5f1a2b35a8adaacace
Unknown <github@vierhauser.net>
2018-10-03 13:18:44 -0400

    getting started project added for dronekit / python and SITL tests

---
+++ gettingstarted/hello_drone.py
@@ -0,0 +1,40 @@
+# Import DroneKit-Python
+from dronekit import connect, VehicleMode, time
+
+#Setup option parsing to get connection string
+import argparse
+parser = argparse.ArgumentParser(description='Print out vehicle state information')
+parser.add_argument('--connect',help="vehicle connection target string.")
+args=parser.parse_args()
+
+connection_string = args.connect
+sitl = None
+
+
+#Start SITL if no connection string specified
+if not connection_string:
+    import dronekit_sitl
+    sitl = dronekit_sitl.start_default()
+    connection_string = sitl.connection_string()
+
+# Connect to the Vehicle.
+#   Set `wait_ready=True` to ensure default attributes are populated before `connect()` returns.
+print "\nConnecting to vehicle on: %s" % connection_string
+vehicle = connect(connection_string, wait_ready=True)
+
+#vehicle.wait_ready('autopilot_version')
+
+#Get some vehicle attributes (state)
+print "Get some vehicle attribute values:"
+print "GPS: %s" % vehicle.gps_0
+print "Battery: %s" % vehicle.battery
+print "Last Heartbeat: %s" % vehicle.last_heartbeat
+print "Is Armable?: %s" % vehicle.system_status.state
+print "Mode: %s" % vehicle.mode.name # settable
+
+# Close vehicle object before exiting script
+vehicle.close()
+
+time.sleep(5)
+
+print("Completed")
